3c0ab164cd12bc69082f03dfc5bf8036dfe427a6
LucasDCoelho <darkalp99@gmail.com>
2023-09-05 13:44:15 -0300

    feat: create summary and start table with history

---
+++ src/components/Summary/Summary.tsx
@@ -0,0 +1,34 @@
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
+import { SummaryContainer, SummuryCard } from "./styles";
+
+
+export function Summary(){
+  return (
+    <SummaryContainer>
+      <SummuryCard>
+        <header>
+          <span>Entradas</span>
+          <ArrowCircleUp  size={24} color="#00875F"/>
+        </header>
+
+        <strong>R$ 17.400,00</strong>
+      </SummuryCard>
+      <SummuryCard>
+        <header>
+          <span>Saída</span>
+          <ArrowCircleDown  size={24} color="#AB222E"/>
+        </header>
+
+        <strong>R$ 17.400,00</strong>
+      </SummuryCard>
+      <SummuryCard variant="green">
+        <header>
+          <span>Total</span>
+          <CurrencyDollar  size={24} color="#FFF"/>
+        </header>
+
+        <strong>R$ 17.400,00</strong>
+      </SummuryCard>
+    </SummaryContainer>
+  )
+}
--- src/pages/Transactions/Transactions.tsx
+++ src/pages/Transactions/Transactions.tsx
@@ -1,9 +1,11 @@
 import { Header } from "../../components/Header/Header";
+import { Summary } from "../../components/Summary/Summary";
 
 export function Transactions(){
   return (
     <>
       <Header />
+      <Summary />
     </>
   )
 }
